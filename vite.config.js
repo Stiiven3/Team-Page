3ddcef967d91a4b184715e5d60ca0e4b186f6b38
Steven <stevenorozco2021@gmail.com>
2025-08-12 02:34:48 -0400

    Practica1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
+  base: '/Team-Page/',
   plugins: [
     tailwindcss(),react() 
   ],
